0e6a6e98cef81413c6ef91a61992811960db303e
rjalamanac <rjalamanac@iescomercio.com>
2026-01-30 18:37:34 +0100

    First commit

---
+++ main.js
@@ -0,0 +1,18 @@
+const elements = document.querySelectorAll(".fade-in");
+
+if (window.innerWidth < 768) {
+  elements.forEach((el) => el.classList.add("visible"));
+} else {
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+        }
+      });
+    },
+    { threshold: 0.2 },
+  );
+
+  elements.forEach((el) => observer.observe(el));
+}
