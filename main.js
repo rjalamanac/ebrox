a59c9f8fc2ebe80ac37a7e8d7bb7d7e7afaad06c
rjalamanac <rjalamanac@iescomercio.com>
2026-03-13 10:50:47 +0100

    Version deployt

--- main.js
+++ main.js
@@ -16,3 +16,20 @@ if (window.innerWidth < 768) {
 
   elements.forEach((el) => observer.observe(el));
 }
+
+// Tabs Modalidades
+document.querySelectorAll("#modalidades .tab").forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const target = btn.getAttribute("data-target");
+    // activar botón
+    document
+      .querySelectorAll("#modalidades .tab")
+      .forEach((b) => b.classList.remove("active"));
+    btn.classList.add("active");
+    // activar panel
+    document
+      .querySelectorAll("#modalidades .tab-panel")
+      .forEach((p) => p.classList.remove("active"));
+    document.querySelector(target).classList.add("active");
+  });
+});
